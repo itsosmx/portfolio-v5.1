0f4cc255941195d59552db5da70743f83db282ec
itsosmx <o.h1277@yahoo.com>
2024-03-07 19:07:53 +0200

    Refactor generateMetadata function to use getProjects instead of getProjectBySlug

--- src/app/projects/[slug]/layout.tsx
+++ src/app/projects/[slug]/layout.tsx
@@ -1,23 +1,23 @@
-import { getProjectBySlug } from "@/lib/actions";
+import { getProjects } from "@/lib/actions";
 import { Metadata } from "next";
 import React from "react";
 
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-  // const data = await getProjectBySlug(params.slug);
-  // console.log(data);
-  return {}
-  // return {
-  //   title: `osmx.me | ${data.title}`,
-  //   description: data.headline,
-  //   keywords: [""],
-  //   openGraph: {
-  //     images: [
-  //       {
-  //         url: data.thumbnail.url
-  //       },
-  //     ],
-  //   },
-  // };
+  const projects = await getProjects();
+  const data = projects?.find((project) => project.slug === params.slug);
+
+  return {
+    title: `osmx.me | ${data?.title}`,
+    description: data?.headline,
+    keywords: [data?.technologies.join(", ") || "", "osmx", "osama hussein", "software engineer"],
+    openGraph: {
+      images: [
+        {
+          url: `${data?.thumbnail.url}`,
+        },
+      ],
+    },
+  };
 }
 
 export default function ProjectSlugLayout({ children }: { children: React.ReactNode }) {
