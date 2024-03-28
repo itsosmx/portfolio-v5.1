de7eaa231b747fec48c149ada2f0a4dda7e03a1f
itsosmx <o.h1277@yahoo.com>
2024-03-28 17:29:34 +0200

    Remove unused imports and commented code

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,5 @@
 import { PageHeader, ProjectCard } from "@/components";
 import { getProjects } from "@/lib/actions";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 export default async function Projects() {
@@ -10,6 +8,13 @@ export default async function Projects() {
   return (
     <div className="lg:p-16 p-4 flex flex-col gap-4">
       <PageHeader className="p-4" title="All Projects" />
+      <div className="flex items-center justify-center">
+        {/* <input
+          type="search"
+          className="rounded-lg p-2 bg-glass-100 outline-none ring-accent ring-1 w-1/2"
+          placeholder="Searching for project with specific technology"
+        /> */}
+      </div>
       {projects?.map((project) => {
         return <ProjectCard project={project} />;
       })}
